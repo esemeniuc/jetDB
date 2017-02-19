6f3d9470118bef0b947b521b84eaee0b383d00b3
cwaffles <cwaffles@users.noreply.github.com>
2017-02-18 20:46:52 -0800

    updated test.cpp

--- src/test.cpp
+++ src/test.cpp
@@ -2,29 +2,34 @@
 #include <pqxx/pqxx>
 
 int main(int argc, char** argv){
-  pqxx::connection c("dbname=company user=accounting");
+  pqxx::connection c("dbname=postgres user=postgres password= hostname=localhost");
   pqxx::work txn(c);
 
   pqxx::result r = txn.exec(
-    "SELECT id "
-    "FROM Employee "
-    "WHERE name =" + txn.quote(argv[1]));
+    "SELECT pid, Model "
+    "FROM Plane "
+    "WHERE Model =" + txn.quote(argv[1]));
 
   if (r.size() != 1)
   {
     std::cerr
-      << "Expected 1 employee with name " << argv[1] << ", "
+      << "Expected 1 plane with name " << argv[1] << ", "
       << "but found " << r.size() << std::endl;
     return 1;
   }
 
-  int employee_id = r[0][0].as<int>();
-  std::cout << "Updating employee #" << employee_id << std::endl;
-
-  txn.exec(
-    "UPDATE EMPLOYEE "
-    "SET salary = salary + 1 "
-    "WHERE id = " + txn.quote(employee_id));
-
-  txn.commit();
+	std::cout << "Found " << r.size() << "   planes:" << std::endl;
+	for (auto row: r)
+	{
+		std::cout << row[1].c_str() << std::endl;
+	}
+//  int employee_id = r[0][0].as<int>();
+//  std::cout << "Updating employee #" << employee_id << std::endl;
+//
+//  txn.exec(
+//    "UPDATE EMPLOYEE "
+//    "SET salary = salary + 1 "
+//    "WHERE id = " + txn.quote(employee_id));
+//
+//  txn.commit();
 }
