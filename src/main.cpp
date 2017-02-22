34787dd5e88e29bfe3702638c03731dfaf6ed8a6
cwaffles <cwaffles@users.noreply.github.com>
2017-02-22 09:39:03 -0800

    initial work towards booking

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+//
+// Created by eric on 2/21/17.
+//
+#include <iostream>
+#include <pqxx/pqxx>
+#include "ring2.h"
+
+int main(int argc, char** argv)
+{
+	pqxx::connection c("dbname=postgres user=postgres password= hostname=localhost");
+	pqxx::work txn(c);
+
+
+	int userinfo = getInfo(txn);
+
+}
