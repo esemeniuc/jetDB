d868a94a7975fae2809e5d0f5dbda8d4cc7310be
cwaffles <cwaffles@users.noreply.github.com>
2017-02-22 17:18:11 -0800

    working data collection

--- test/getFlights.sql
+++ test/getFlights.sql
@@ -1,13 +1,40 @@
 --get flights
-SELECT * FROM flight
+SELECT CONCAT(flight.prefix, flightname.flightnum) AS flightname,
+airline.alname, flight.starttime, flight.endtime FROM flight
 JOIN named ON named.fid = flight.fid --needed for getting the human name of the flight
 JOIN flightname ON flightname.prefix = named.prefix AND flightname.flightnum = named.flightnum
+JOIN airline ON airline.prefix = flight.prefix
+WHERE fromairportcode = 'JFK'
+AND toairportcode = 'YVR'
+AND starttime > '2015-01-01 00:00:00'
+AND endtime < '2017-01-01 00:00:00';
 
--- JOIN airline ON airline.prefix = flight.prefix --need prefix for flightname
-
--- NATURAL JOIN airport --not needed because the user provides the code
--- WHERE flight.starttime = ''
--- AND flight.endtime = ''
+--get flights natural join
+SELECT CONCAT(flight.prefix, flightname.flightnum) AS flightname,
+airline.alname, flight.starttime, flight.endtime FROM flight
+NATURAL JOIN named --needed for getting the human name of the flight
+NATURAL JOIN flightname
+NATURAL JOIN airline
+WHERE fromairportcode = 'JFK'
+AND toairportcode = 'YVR'
+AND starttime > '2015-01-01 00:00:00'
+AND endtime < '2017-01-01 00:00:00';
 
 --get airports
-SELECT * FROM airport
+-- SELECT * FROM airport
+
+--2016-10-15 23:39:37.000000
+--2016-10-16 23:39:37.000000
+
+SELECT
+  CONCAT(flight.prefix, flightname.flightnum) AS flightname,
+  airline.alname,
+  flight.starttime,
+  flight.endtime
+FROM
+  flight
+  NATURAL JOIN named
+  NATURAL JOIN flightname
+  NATURAL JOIN airline
+WHERE fromairportcode = 'JFK' AND toairportcode = 'YVR' AND starttime >= '2016-10-15 23:39:37.000000' AND
+      endtime <= '2016-10-16 23:39:37.000000'
