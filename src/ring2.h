a76eb4e1bd3a30ece39a3be4a7f03c5194c8cd98
cwaffles <cwaffles@users.noreply.github.com>
2017-02-22 22:31:01 -0800

    add date capture

--- src/ring2.h
+++ src/ring2.h
@@ -5,9 +5,9 @@ int bookFlightByID(std::string GovID, int DateBooked, std::vector<int> flightID)
 
 bool isValid(std::string validateString, pqxx::work& txn);
 
-pqxx::result::size_type lookupPrint(std::string lookupString, pqxx::work& txn);
-
 void printResult(pqxx::result& printResult);
+
+pqxx::result::size_type lookupPrint(std::string lookupString, pqxx::work& txn);
 
 std::string getUserInput(pqxx::work& txn,
 						 std::string promptUserString,
@@ -15,4 +15,6 @@ std::string getUserInput(pqxx::work& txn,
 						 std::string validationString,
 						 int numParamsForValidation);
 
-int getInfo(pqxx::work& txn);
+std::string getDate(std::string promptUserString);
+
+int getBookingInfo(pqxx::work& txn);
