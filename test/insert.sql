9b1b23d7bfc979082e90a625cc50e5e02277a4c2
cwaffles <cwaffles@users.noreply.github.com>
2017-02-25 19:55:00 -0800

    updated sql files

--- test/insert.sql
+++ test/insert.sql
@@ -14,3 +14,9 @@
 -- DECLARE bookingID SERIAL;
 
 INSERT INTO booking (bid) VALUES (DEFAULT) RETURNING bid;
+--INSERT INTO booking (bid) VALUES (nextval('booking_bid_seq'::regclass)) RETURNING bid; --alterante
+
+INSERT INTO books (govid, bid, datebooked, cost) VALUES ($1, $2, $3, $4) RETURNING books;
+
+--get total cost of flight
+SELECT SUM(Cost) FROM Flight WHERE fid IN ('1' '2' '3' );
