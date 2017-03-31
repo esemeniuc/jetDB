49f5263b8c2a2e406df81d90b993dd0798f2ed81
Ming-Tang <project.shinkirou@gmail.com>
2017-03-31 00:15:26 -0700

    Enforce starttime/endtime ordering in pathfinding

--- include/handlers/find_paths.hpp
+++ include/handlers/find_paths.hpp
@@ -11,19 +11,8 @@ namespace jetdb{
       std::string to_airportcode = req.to_airportcode;
       txn.conn().prepare(
         "find_paths",
-        "WITH RECURSIVE pathfind(fromairportcode, toairportcode, last_fid, last_cost, cost, path, fids, cycle, depth)"
-        " as ("
-        "SELECT fromairportcode, toairportcode, fid, cost, cost, ARRAY[upper(fromairportcode)], ARRAY[fid], false, 0 from flight f"
-        " UNION ALL "
-        "SELECT f.fromairportcode, f.toairportcode, f.fid, f.cost, f.cost+sf.cost,"
-        " path || upper(f.fromairportcode), fids || f.fid, upper(f.fromairportcode) = ANY(path), sf.depth+1"
-        " FROM flight f, pathfind sf "
-        "WHERE NOT cycle AND f.fromairportcode = sf.toairportcode"
-        "), paths AS ("
-        "SELECT fromairportcode,toairportcode,last_fid,last_cost,cost,path || upper(toairportcode) as complete_path,"
-        " fids,cycle,depth, path[1] as src, toairportcode as dst"
-        " FROM pathfind ORDER BY src, dst, cost DESC)"
-        " select * from paths where NOT CYCLE and src=$1 and dst=$2;"
+        "with recursive pathfind(fromairportcode, toairportcode, pathstarttime, pathendtime, last_fid, last_cost, cost, path, fids, cycle, depth) as (select fromairportcode, toairportcode, starttime, endtime, fid, cost, cost, ARRAY[upper(fromairportcode)], ARRAY[fid], false, 0 from flight f union all select f.fromairportcode, f.toairportcode, sf.pathstarttime, f.endtime, f.fid, f.cost, f.cost+sf.cost, path || upper(f.fromairportcode), fids || f.fid, upper(f.fromairportcode) = ANY(path), sf.depth+1 from flight f, pathfind sf where NOT cycle AND f.fromairportcode = sf.toairportcode AND f.starttime > sf.pathendtime), paths as (SELECT fromairportcode, toairportcode, pathstarttime, pathendtime, last_fid,last_cost,cost,path || upper(toairportcode) as complete_path, fids, cycle, depth, path[1] as src, toairportcode as dst FROM pathfind WHERE TRUE OR (NOT cycle AND NOT(toairportcode = ANY(path))) ORDER BY src, dst, cost DESC) select * from paths"
+        " where NOT CYCLE and src=$1 and dst=$2;"
       );
       result = txn.prepared("find_paths")(from_airportcode)(to_airportcode).exec();
 
